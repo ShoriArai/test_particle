21f3f885767c4ab6ea06ce39fc133eaa7a8633bf
ShoriArai <24wm2101@student.gs.chiba-u.jp>
2025-03-28 01:02:53 +0900

    Add files via upload

--- test_particle.py
+++ test_particle.py
@@ -1,6 +1,9 @@
-#変動する電場と一定の磁場のもとで、
-#相対論的な速度を持つ電気を帯びた粒子の運動を数値的に解きつつ、
-#結果をグラフで出力するプログラム
+"""
+変動する電場と一定の磁場のもとで、
+相対論的な速度を持つ電気を帯びた粒子の運動を数値的に解きつつ、
+結果をグラフで出力するプログラム
+"""
+
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
@@ -19,30 +22,29 @@ def equation(t, y):
     v_vec = p_vec / (gamma * m)
     
     # ローレンツ力
-    #dpdt = q * (E + np.cross(v_vec, B))
     E_dynamic = E_field(x, y_pos)
     qbmc = q*B/(m*c)
     
     return v_vec[0], v_vec[1], qbmc * (E_dynamic + v_vec[1]), -qbmc * (v_vec[0] + beta)
 
 #よく変更するパラメータ
-t0 = 600        #時間
-gamma0 = 40     #ローレンツ因子
-E0 = 0.15  # 電場の振幅
-k = 1/600  # 空間変化の係数
-sigma = 16.834    #磁場の強さを決める値
+t0 = 600                    #時間
+gamma0 = 40                 #ローレンツ因子
+E0 = 0.15                   #電場の振幅
+k = 1/600                   #空間変化の係数
+sigma = 16.834              #磁場の大きさを決めるパラメータ
+q = -0.00446                #電荷
 gname = "test_particle.png" #保存する画像の名前
 gformat = "png"             #ファイル形式
 gdpi = 300                  #画像のdpi
 
 #基本的に固定のパラメータ
-delx = 1
+delx = 1        #領域の刻み幅
 m = 1           #質量
 c = 1           #光速
-q = -0.00446    #電荷
 beta = np.sqrt((gamma0**2-1))/gamma0
 v0 = -c*beta    #初速度
-N1 = 100    #粒子数
+N1 = 100        #粒子数
 B  = np.sqrt(4*np.pi*m*gamma0*sigma*N1/delx) * c    #磁場
 time = np.linspace(0,t0,6000)   #時間
 
